cb3a1f9c80afd3aac057a249ceb1886335852cdf
Loc Tran <tranhuuloc05@gmail.com>
2026-04-28 23:51:58 +0700

    Fix layout

--- src/main/java/hcmute/edu/vn/fashionstore/controller/admin/AdminController.java
+++ src/main/java/hcmute/edu/vn/fashionstore/controller/admin/AdminController.java
@@ -10,12 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 @Controller
-@RequestMapping("/admin") // Gốc đường dẫn là /admin
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
